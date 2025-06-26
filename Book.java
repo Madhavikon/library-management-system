08838e982499d9215608ba736637898dcb631e90
Madhavikon <163968372+Madhavikon@users.noreply.github.com>
2025-06-26 18:39:35 +0530

    Create Book.java

---
+++ Book.java
@@ -0,0 +1,32 @@
+// Book.java
+public class Book {
+    private String title;
+    private String author;
+    private boolean isIssued;
+
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+        this.isIssued = false;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public boolean isIssued() {
+        return isIssued;
+    }
+
+    public void issue() {
+        isIssued = true;
+    }
+
+    public void returnBook() {
+        isIssued = false;
+    }
+}
